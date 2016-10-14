a7cefdafc747a9b861caf34136c9c697015d0c40
Jan Koscielniak <jkosci@gmail.com>
2016-10-14 11:32:46 +0200

    Initial commit
    Adding files from assignment

---
+++ fpga/ledc8x8.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity ledc8x8 is
+port ( -- Sem doplnte popis rozhrani obvodu.
+
+);
+end ledc8x8;
+
+architecture main of ledc8x8 is
+
+    -- Sem doplnte definice vnitrnich signalu.
+
+begin
+
+    -- Sem doplnte popis funkce obvodu (zakladni konstrukce VHDL jako napr.
+    -- prirazeni signalu, multiplexory, dekodery, procesy...).
+    -- DODRZUJTE ZASADY PSANI SYNTETIZOVATELNEHO VHDL UVEDENE NA WEBU:
+    -- http://merlin.fit.vutbr.cz/FITkit/docs/navody/synth_templates.html
+
+    -- Nezapomente take doplnit mapovani signalu rozhrani na piny FPGA
+    -- v souboru ledc8x8.ucf.
+
+end main;
