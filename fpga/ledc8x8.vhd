bc8de635067ec667eb3b29b8d76e9bf7b41d4dc9
Jan Koscielniak <jkosci@gmail.com>
2016-10-25 19:25:52 +0200

    Final touches

--- fpga/ledc8x8.vhd
+++ fpga/ledc8x8.vhd
@@ -16,16 +16,14 @@ architecture main of ledc8x8 is
 
     -- Sem doplnte definice vnitrnich signalu.
     signal row_sig: std_logic_vector(7 downto 0) := "10000000";
-    signal led_sig: std_logic_vector(7 downto 0) := "00000000";
     signal clk: std_logic := '0';
     signal clk_cnt: std_logic_vector(7 downto 0) := "00000000";
     signal first: std_logic := '0';
-    signal row_cnt: std_logic_vector(3 downto 0) := "0000";
+    signal row_cnt: std_logic_vector(15 downto 0) := "0000000000000000";
     signal J_in: std_logic_vector(7 downto 0);
     signal K_in: std_logic_vector(7 downto 0);    
     signal J_out: std_logic_vector(7 downto 0);
     signal K_out: std_logic_vector(7 downto 0);
-    signal Y: std_logic_vector(7 downto 0);
 begin
 
     -- Sem doplnte popis funkce obvodu (zakladni konstrukce VHDL jako napr.
@@ -35,7 +33,6 @@ begin
 
     -- Nezapomente take doplnit mapovani signalu rozhrani na piny FPGA
     -- v souboru ledc8x8.ucf.
-    -- rotacni registr
     clk_gen: process(RESET,SMCLK)
     begin
     	if (RESET = '1') then	
@@ -62,61 +59,62 @@ begin
 	   end if;
 	end process;
 
-	demux: process(row_sig) 	
+	demux: process(row_sig, first, SMCLK, clk) 	
 	begin
-	   case first is
-	      when '0' => J_in <= row_sig;
-	      when '1' => K_in <= row_sig;
-	      when others => J_in <= row_sig;
-	   end case;
+		if (SMCLK'event) and (SMCLK = '1') then
+		   case first is
+		      when '0' => J_in <= row_sig;
+		      when '1' => K_in <= row_sig;
+		      when others => J_in <= row_sig;
+		   end case;
+		end if;
 	end process;
 
-	mux: process(J_out, K_out)
+	j_decoder: process(J_in, SMCLK, clk)
 	begin
-	   case first is
-	      when '0' => Y <= J_out;
-	      when '1' => Y <= K_out;
-	      when others => Y <= J_out;
-	   end case;
+		if (SMCLK'event) and (SMCLK = '1')then
+		   case J_in is
+		      when "10000000" => J_out <= "11111101";
+		      when "01000000" => J_out <= "11111101";
+		      when "00100000" => J_out <= "11111101";
+		      when "00010000" => J_out <= "11111101";
+		      when "00001000" => J_out <= "11111101";
+		      when "00000100" => J_out <= "10111101";
+		      when "00000010" => J_out <= "10111101";
+		      when "00000001" => J_out <= "11000011";
+		      when others => J_out <= "01111110";
+		   end case;
+	    end if;
 	end process;
 
-	j_decoder: process(J_in)
+	k_decoder: process(K_in, SMCLK, clk)
 	begin
-	   case J_in is
-	      when "10000000" => J_out <= "11110111";
-	      when "01000000" => J_out <= "11110111";
-	      when "00100000" => J_out <= "11110111";
-	      when "00010000" => J_out <= "10110111";
-	      when "00001000" => J_out <= "11001111";
-	      when "00000100" => J_out <= "11111111";
-	      when "00000010" => J_out <= "11111111";
-	      when "00000001" => J_out <= "11111111";
-	      when others => J_out <= "11111111";
-	   end case;
+		if (SMCLK'event) and (SMCLK = '1') then
+		    case K_in is
+		      when "10000000" => K_out <= "01110111";
+		      when "01000000" => K_out <= "01101111";
+		      when "00100000" => K_out <= "01011111";
+		      when "00010000" => K_out <= "00111111";
+		      when "00001000" => K_out <= "00111111";
+		      when "00000100" => K_out <= "01011111";
+		      when "00000010" => K_out <= "01101111";
+		      when "00000001" => K_out <= "01110111";
+		      when others => K_out <= "11111000";
+		   	end case;
+   		end if;
 	end process;
-
-	k_decoder: process(K_in)
-	begin
-	   case K_in is
-	      when "10000000" => K_out <= "01101111";
-	      when "01000000" => K_out <= "01011111";
-	      when "00100000" => K_out <= "00111111";
-	      when "00010000" => K_out <= "01011111";
-	      when "00001000" => K_out <= "01101111";
-	      when "00000100" => K_out <= "11111111";
-	      when "00000010" => K_out <= "11111111";
-	      when "00000001" => K_out <= "11111111";
-	      when others => K_out <= "11111111";
-	   end case;
-	end process;
-	write: process(SMCLK, Y, clk)
+	write: process(SMCLK, clk)
 	begin
 	   if (SMCLK'event) and (SMCLK = '1') and (clk = '1')then
 			ROW <= row_sig;
-			LED <= Y;
+		   case first is
+		      when '0' => LED <= J_out;
+		      when '1' => LED <= K_out;
+		      when others => led <= J_out;
+		   end case;
 			row_cnt <= row_cnt + 1;
-			if (row_cnt = 7) then
-				row_cnt <= "0000";
+			if (row_cnt = "0111111111111111") then
+				row_cnt <= "0000000000000000";
 				first <= first nand first;
 			end if ;
 	   end if;
